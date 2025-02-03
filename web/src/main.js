564370ac5ed90752d185927c7f67adb0d5996a12
Afif Arman <afifarman50@gmail.com>
2025-02-03 23:57:40 +0700

    refactor: add remove file btn

--- web/src/main.js
+++ web/src/main.js
@@ -18,13 +18,14 @@ toastr.options.extendedTimeOut = 1000; // How long the toast will display after 
 toastr.options.progressBar = true;
 $("#app").html(
   `<main class="flex mx-auto flex-col m-2 w-1/2">
-    <div class="flex border-2 gap-2 px-2 py-2 pb-2 flex-col border-black border-solid w-full h-60 mb-2 overflow-y-auto" id="txtContainer">
+    <div class="flex border-2 gap-2 p-2 pb-2 flex-col border-black border-solid w-full overflow-x-hidden overflow-y-auto h-60 mb-2" id="txtContainer">
     </div>
     <div class="flex flex-col items-center justify-center w-full">
       <form class="flex flex-col w-full" id="formUpload">
         <textarea value="Gollama" disabled id="txt" class="focus:border-blue-500 mb-2 flex w-full border-solid border-2 border-black rounded-lg p-2" name="txt" placeholder="Gollama"></textarea>
         <div class="grid grid-cols-2 gap-2 flex-row w-full">
           <input type="file" id="file" name="file" class="w-full p-2  bg-blue-100 rounded-lg hover:border-solid hover:border-2 hover:border-blue-100 hover:bg-white hover:text-black cursor-not-allowed">
+          <button class='flex bg-green-500 text-white w-full p-2 rounded-lg hover:border-solid hover:border-2 hover:border-green-100 hover:bg-white hover:text-black' id='rmvfl' type='button'>Remove file</button>
           <button type="submit" disabled id="send" class="flex w-full p-2 bg-red-500 cursor-not-allowed rounded-lg hover:bg-white hover:border-solid hover:border-red-500 hover:border-2 hover:text-black text-white justify-center">send</button>
         </div>
       </form>
@@ -54,6 +55,7 @@ let data = {};
 data.txt = $("#txt").val();
 $("#txt").addClass("cursor-not-allowed");
 $("#file").attr("disabled", true);
+$("#rmvfl").hide();
 $("#file").on("change", function (e) {
   let fileList = e.target.files;
   if (!fileList.length) return;
@@ -62,6 +64,25 @@ $("#file").on("change", function (e) {
   $("#send").addClass("cursor-pointer");
   $("#txt").removeClass("cursor-not-allowed");
   $("#txt").attr("disabled", false);
+  $("#file").parent().removeClass("grid-cols-2");
+  $("#file").parent().addClass("grid-cols-3");
+  $("#file").addClass("cursor-not-allowed");
+  $("#rmvfl").show();
+  $("#rmvfl").addClass("cursor-pointer");
+  $("#file").attr("disabled", true);
+});
+
+$("#rmvfl").on("click", function (e) {
+  e.preventDefault();
+  formData.delete("file");
+  $("#file").val("");
+  $("#file").attr("disabled", false);
+  $("#file").removeClass("cursor-not-allowed");
+  $("#file").addClass("cursor-pointer");
+  toastr.success("successfully remove file");
+  $("#rmvfl").hide();
+  $("#file").parent().removeClass("grid-cols-3");
+  $("#file").parent().addClass("grid-cols-2");
 });
 
 let dataModelsArray = [];
@@ -130,10 +151,16 @@ $("#btnHtUrl").on("click", function (e) {
 
 $(".basic-single-embed").on("change", function (e) {
   const value = e.target.value;
+  if (formData.has("modelEmbed")) {
+    formData.delete("modelEmbed");
+  }
   formData.append("modelEmbed", value);
 });
 $(".basic-single-chat").on("change", function (e) {
   const value = e.target.value;
+  if (formData.has("modelChat")) {
+    formData.delete("modelChat");
+  }
   formData.append("modelChat", value);
 });
 
@@ -151,8 +178,9 @@ $("#formUpload").on("submit", function (e) {
 
   formData.append("txt", data.txt);
   $("#txtContainer").append(
-    '<h6 class="flex bg-gray-100 w-max px-2 py-1 rounded-lg">👤: ' +
+    '<h6 class="flex bg-gray-100 w-full px-2 py-1 rounded-lg break-all">' +
       data.txt +
+      " - 👤" +
       "</h6>",
   );
 
@@ -171,9 +199,7 @@ $("#formUpload").on("submit", function (e) {
       $("#btnHtUrl").attr("disabled", true);
       $("#send").attr("disabled", true);
       $("#txt").attr("disabled", true);
-
       toastr.info("Info fetching");
-
       $("body").css("opacity", 0.5);
       $("body").css("cursor", "not-allowed");
       $("body").css("cursor", "not-allowed");
@@ -182,6 +208,7 @@ $("#formUpload").on("submit", function (e) {
       $("main").css("left", "30%");
       $("body").css("z-index", 999);
       $("#file").attr("disabled", true);
+      $("#rmvfl").attr("disabled", true);
       $("body").css(
         "background-image",
         `url("data:image/svg+xml,%3Csvg width='24' height='24' stroke='%23000' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'%3E%3Cstyle%3E.spinner_V8m1%7Btransform-origin:center;animation:spinner_zKoa 2s linear infinite%7D.spinner_V8m1 circle%7Bstroke-linecap:round;animation:spinner_YpZS 1.5s ease-in-out infinite%7D@keyframes spinner_zKoa%7B100%25%7Btransform:rotate(360deg)%7D%7D@keyframes spinner_YpZS%7B0%25%7Bstroke-dasharray:0 150;stroke-dashoffset:0%7D47.5%25%7Bstroke-dasharray:42 150;stroke-dashoffset:-16%7D95%25,100%25%7Bstroke-dasharray:42 150;stroke-dashoffset:-59%7D%7D%3C/style%3E%3Cg class='spinner_V8m1'%3E%3Ccircle cx='12' cy='12' r='9.5' fill='none' stroke-width='3'%3E%3C/circle%3E%3C/g%3E%3C/svg%3E%0A"`,
@@ -195,6 +222,9 @@ $("#formUpload").on("submit", function (e) {
       $("body").removeAttr("style");
       $("main").removeAttr("style");
       $("body *").removeAttr("disabled");
+      if (formData.has("txt")) {
+        formData.delete("txt");
+      }
     },
     success: function (data, textStatus, jqXHR) {
       $("body *").removeAttr("disabled");
@@ -206,10 +236,12 @@ $("#formUpload").on("submit", function (e) {
       $("#btnHtUrl").attr("disabled", false);
       $("#send").attr("disabled", false);
       $("#txt").attr("disabled", false);
+      $("#rmvfl").attr("disabled", false);
       toastr.success("successfully response fetch");
       $("#txtContainer").append(
-        '<h6 class="flex bg-amber-100 w-max px-2 py-1 rounded-lg justify-end">🤖: ' +
+        '<h6 class="flex bg-amber-100 w-full px-2 py-1 rounded-lg break-all">' +
           data.message +
+          "- 🤖" +
           "</h6>",
       );
     },
@@ -218,6 +250,7 @@ $("#formUpload").on("submit", function (e) {
       if (textStatus == "error") {
         $("body *").removeAttr("disabled");
         $("#txt").attr("disabled", false);
+        $("#rmvfl").attr("disabled", false);
         $("main").removeAttr("style");
         $("body").removeAttr("style");
         const jsonParseMessage = JSON.parse(jqXhr.responseText.trim());
