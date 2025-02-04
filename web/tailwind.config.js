f56988615b433ac142c15c409effd109cd069d3d
Afif Arman <afifarman50@gmail.com>
2025-02-05 01:13:34 +0700

    refactor: need edit again

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -3,8 +3,8 @@ export default {
   theme: {
     extend: {},
     screens: {
-      "min-sm": { min: "300px", max: "600px" },
-      sm: { min: "640px", max: "767px" },
+      "min-sm": { min: "300px", max: "500px" },
+      sm: { min: "500px", max: "767px" },
       md: { min: "768px", max: "1023px" },
       lg: { min: "1024px", max: "1279px" },
       xl: { min: "1280px" },
