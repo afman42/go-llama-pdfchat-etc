586f92d1b0281c0d46414781f16b7e394a182bec
Afif Arman <afifarman50@gmail.com>
2025-02-06 10:31:10 +0700

    refactor: add func read ip

--- utils/helper.go
+++ utils/helper.go
@@ -95,3 +95,15 @@ func PathFileTemp(filename string) string {
 	dir = dir + "/tmp/"
 	return filepath.Join(dir + filename)
 }
+
+// https://stackoverflow.com/questions/27234861/correct-way-of-getting-clients-ip-addresses-from-http-request
+func ReadUserIP(r *http.Request) string {
+	IPAddress := r.Header.Get("X-Real-Ip")
+	if IPAddress == "" {
+		IPAddress = r.Header.Get("X-Forwarded-For")
+	}
+	if IPAddress == "" {
+		IPAddress = r.RemoteAddr
+	}
+	return IPAddress
+}
